d24c392ddfb47ac6186d97fa8d825a8aff0219f1
Serge Fonville <serge.fonville@gmail.com>
2018-09-13 00:12:24 +0200

    Started switching to Flask

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import os
 # Change working directory so relative paths (and template lookup) work again
 os.chdir(os.path.dirname(__file__))
 
-from bottle import *
-# ... build or import your bottle application here ...
-# Do NOT use bottle.run() with mod_wsgi
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 @route('/')
 def home():
