53a8e22ae6c381945bcd8c8359285f1e18b02aca
Serge Fonville <serge.fonville@gmail.com>
2018-09-13 15:55:52 +0200

    Renamed app to application
    It seems counter-intuitive to rename a vairable during import,
    it makes sense to do it at the latest point possible,
    since it is more readable and includes less typing when using it
    up to that point.

--- MealRegistration/__init__.py
+++ MealRegistration/__init__.py
@@ -1,23 +1,23 @@
 import os
 from flask import Flask, send_from_directory
 applicationDirectory = os.path.dirname(__file__)
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def home():
     return send_from_directory('{0}/{1}'.format(applicationDirectory,'static'), 'index.html')
 
-@app.route('/css/<path:filename>')
+@application.route('/css/<path:filename>')
 def css(filename):
   return send_from_directory('{0}/{1}/{2}'.format(applicationDirectory,'static', 'css'), filename)
 
-@app.route('/scripts/<path:filename>')
+@application.route('/scripts/<path:filename>')
 def scripts(filename):
   return send_from_directory('{0}/{1}/{2}'.format(applicationDirectory,'static', 'scripts') + '', filename)
 
-@app.route('/templates/<path:filename>')
+@application.route('/templates/<path:filename>')
 def templates(filename):
   return send_from_directory('{0}/{1}/{2}'.format(applicationDirectory,'static', 'templates'), filename)
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
 
-from MealRegistration import app as application
+from MealRegistration import application
 application.secret_key = '_XR/(!I"BvsRc+*.gPRs(<yD7K99Z!K:H2,//&%"=O5cL<K9j80O^GhnliI>a/_G'
