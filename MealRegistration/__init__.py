6f09db9dc6d5a34244bb680c2af8e52341ad2cec
Serge Fonville <serge.fonville@gmail.com>
2018-09-13 17:15:55 +0200

    Aligned spacing

--- MealRegistration/__init__.py
+++ MealRegistration/__init__.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return send_from_directory('{0}/{1}'.format(applicationDirectory,'static'), 'index.html')
+  return send_from_directory('{0}/{1}'.format(applicationDirectory,'static'), 'index.html')
 
 @application.route('/css/<path:filename>')
 def css(filename):
@@ -20,4 +20,4 @@ def templates(filename):
   return send_from_directory('{0}/{1}/{2}'.format(applicationDirectory,'static', 'templates'), filename)
 
 if __name__ == "__main__":
-    application.run()
+  application.run()
