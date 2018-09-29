5ca298bd24b3e2bcc23706689b827c70e176af99
Serge Fonville <serge.fonville@gmail.com>
2018-09-29 17:30:56 +0200

    Added api blueprint

--- api/controllers.py
+++ api/controllers.py
@@ -2,3 +2,19 @@ from flask import Blueprint
 from shared.database import db
 
 api = Blueprint('api', __name__, url_prefix='/api')
+
+
+@api.route("/meal/add", methods=['POST'])
+def addMeal():
+    pass
+
+
+@api.route("/meal/find")
+def findMeal():
+    pass
+
+
+@api.route("/fooditem/find")
+def findFoodItem():
+    pass
+
--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from flask import Flask
 from shared.database import db
 from config import Config
 from main.controllers import main
+from api.controllers import api
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(main)
+app.register_blueprint(api)
 db.init_app(app)
 
 if __name__ == '__main__':
