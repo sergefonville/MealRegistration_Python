c56da83f86b81f7f3339f421ac268fa4f211dab9
Serge Fonville <serge.fonville@gmail.com>
2018-09-30 00:14:34 +0200

    Started implementing controller functions
    The main challenge at this point seems to be how to
    limit the possible values that can be assigned to the unit property
    of a MealPart instance and allow direct assignment of a string value
    to the property, while still maintaining the normalized model

--- api/controllers.py
+++ api/controllers.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask import jsonify
 from shared.database import db
+from shared.models import Meal, FoodItem, Unit
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
@@ -11,10 +13,26 @@ def addMeal():
 
 @api.route("/meal/find")
 def findMeal():
-    pass
+    q = request.args.get('q',)
+    meals = Meal.query.first()
+    return jsonify(meals)
 
 
 @api.route("/fooditem/find")
 def findFoodItem():
-    pass
+    q = request.args.get('q',)
+    fooditem = FoodItem.query.first()
+    return jsonify(fooditem)
 
+
+@api.route('/fooditem/add', methods=['POST'])
+def addFoodItem():
+    fooditem = FoodItem()
+    unit = Unit()
+    unit.name = request.args.get('unit')
+    fooditem.name = request.args.get('name')
+    fooditem.calories_per_unit = request.args.get('calories_per_unit')
+    fooditem.carbohydrates_per_unit = request.args.get(
+        'carbohydrates_per_unit'
+    )
+    fooditem.unit = request.args.get('calories_per_unit')
