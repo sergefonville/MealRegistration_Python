567486a7c0bf9303d548971bc34a4d5cdbc35534
Serge Fonville <serge.fonville@gmail.com>
2018-09-29 22:25:11 +0200

    Create normalizable classes
    First attempt to model the classes so the resulting database is
    properly normalized, without loosing the ORM functionality.

--- shared/models.py
+++ shared/models.py
@@ -3,16 +3,28 @@ from shared.database import db
 
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime)
-    foodItems = db.relationship('FoodItem')
+    timestamp = db.Column(db.DateTime, nullable=False)
+    mealparts = db.relationship('MealPart')
 
 
 class FoodItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False, unique=True)
     unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'), nullable=False)
-    carbohydratesPerUnit = db.Column(db.Integer, nullable=False)
-    caloriesPerUnit = db.Column(db.Integer, nullable=False)
+    carbohydrates_per_unit = db.Column(db.Integer, nullable=False)
+    calories_per_unit = db.Column(db.Integer, nullable=False)
+    mealparts = db.relationship('MealPart')
+
+
+class MealPart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    meal_id = db.Column(db.Integer, db.ForeignKey('meal.id'), nullable=False)
+    fooditem_id = db.Column(
+        db.Integer,
+        db.ForeignKey('food_item.id'),
+        nullable=False
+    )
+    amount = db.Column(db.Integer, nullable=False)
 
 
 class Unit(db.Model):
