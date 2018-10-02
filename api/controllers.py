c0ced80b9525f3e6b7ef7a76910c481876f735e8
Serge Fonville <serge.fonville@gmail.com>
2018-10-02 22:13:25 +0200

    Cleaned controllers
    Made the controllers more consistent and clean. Did some updates to the
    addFoodItem methods, tried to make it possible to assign a string to a
    property and implicitly fill the relation, still not quite sure how to
    do this, the problem with this approach seems to be that for both
    getting and setting a lot of roundtrips to the database are  needed,
    which does not scale.

--- api/controllers.py
+++ api/controllers.py
@@ -6,33 +6,52 @@ from shared.models import Meal, FoodItem, Unit
 api = Blueprint('api', __name__, url_prefix='/api')
 
 
-@api.route("/meal/add", methods=['POST'])
+@api.route("/meal/add", methods=['PUT'])
 def addMeal():
     pass
 
 
-@api.route("/meal/find")
-def findMeal():
+@api.route("/meal/update", methods=['POST'])
+def updateMeal():
+    pass
+
+
+@api.route("/meal/search")
+def searchMeal():
     q = request.args.get('q',)
     meals = Meal.query.first()
     return jsonify(meals)
 
 
-@api.route("/fooditem/find")
-def findFoodItem():
-    q = request.args.get('q',)
-    fooditem = FoodItem.query.first()
+@api.route("/fooditem/search")
+def searchFoodItem():
+    q = request.args.get('q', '')
+    fooditem = FoodItem.query.filter_byfirst()
     return jsonify(fooditem)
 
 
-@api.route('/fooditem/add', methods=['POST'])
+@api.route('/fooditem/add', methods=['PUT'])
 def addFoodItem():
     fooditem = FoodItem()
-    unit = Unit()
+    unit = Unit.query(Unit.name).filter_by(
+        Unit.name.like(
+            f"%{request.args.get('unit')}%"
+        )
+    ).scalar()
+    if unit is None:
+        unit = Unit()
+        unit.name = request.args.get('unit')
+        db.session.add(unit)
+        db.session.commit()
     unit.name = request.args.get('unit')
     fooditem.name = request.args.get('name')
     fooditem.calories_per_unit = request.args.get('calories_per_unit')
     fooditem.carbohydrates_per_unit = request.args.get(
         'carbohydrates_per_unit'
     )
-    fooditem.unit = request.args.get('calories_per_unit')
+    fooditem.unit = unit
+
+
+@api.route('/fooditem/update', methods=['POST'])
+def updateFoodItem():
+    pass
